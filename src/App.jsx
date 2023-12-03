8c6510876ffb45504bd2b4cff0d91810095cbd6c
Gourav <gouravkamble9@gmail.com>
2023-12-03 10:36:58 +0530

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import ReactSwiper from './components/ReactSwiper'
+import ReactSwiper1 from './components/ReactSwiper1'
 
 const App = () => {
   return (
     <div className='App'>
       <div className='swiper-container'>
       <ReactSwiper/>
+      <ReactSwiper1/>
       </div>
     </div>
   )
---
+++ src/components/ReactSwiper1.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './ReactSwiper1.css'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import data1 from '../utils/data1.json'
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+export default function ReactSwiper1() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {
+           data1.map((item,id)=>(
+
+               <SwiperSlide>
+                <img src={item.img} alt="img" style={{width:"400px"}}/>
+               </SwiperSlide>
+           )) 
+        }
+      </Swiper>
+    </>
+  );
+}
